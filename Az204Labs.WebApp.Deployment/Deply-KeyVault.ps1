b15cc7e433690362e8144ead1e4a1735a160fd49
Daniel Ryhle <daniel@ryhle.se>
2020-08-16 21:40:07 +0200

    sa

--- Az204Labs.WebApp.Deployment/Deply-KeyVault.ps1
+++ Az204Labs.WebApp.Deployment/Deply-KeyVault.ps1
@@ -13,13 +13,17 @@ $vault = New-AzKeyVault -VaultName "az-204-labs-webapp-vault" -ResourceGroupName
     -EnabledForDeployment `
     -EnabledForTemplateDeployment `
 
-# Ge behörigheter att hantera keyvault - https://docs.microsoft.com/en-us/powershell/module/azurerm.keyvault/set-azurermkeyvaultaccesspolicy?view=azurermps-6.13.0
+# Ge behörigheter till azure user (en själv) att hantera keyvault - https://docs.microsoft.com/en-us/powershell/module/azurerm.keyvault/set-azurermkeyvaultaccesspolicy?view=azurermps-6.13.0
 $upn = Get-AzADUser
 Set-AzKeyVaultAccessPolicy -VaultName "az-204-labs-webapp-vault" -UserPrincipalName $upn.UserPrincipalName `
     -PermissionsToKeys get,list,update,create,import,delete,recover,backup,restore `
     -PermissionsToSecrets get,list,set,delete,recover,backup,restore,purge `
     -PermissionsToCertificates purge,get,list,update,create,import,delete,recover,backup,restore,managecontacts
 
-$secretValue = ConvertTo-SecureString -String "test123" -AsPlainText -Force
+$secretValue = ConvertTo-SecureString -String "hemlighet från key vault" -AsPlainText -Force
 $secret = Set-AzKeyVaultSecret -VaultName "az-204-labs-webapp-vault" -Name "TestSecret" -SecretValue $secretValue 
 $secret.Id
+
+## Ge get behörighet över secrets för app id som motsvarar vår webapps app registration i Azure
+Set-AzKeyVaultAccessPolicy -VaultName "az-204-labs-webapp-vault" -ServicePrincipalName "120238c9-7552-4f43-97e8-59006afd87ed" `
+    -PermissionsToSecrets get
--- Az204Labs.WebApp/Pages/Index.cshtml.cs
+++ Az204Labs.WebApp/Pages/Index.cshtml.cs
@@ -8,12 +8,35 @@ using Microsoft.Extensions.Logging;
 
 namespace Az204Labs.WebApp.Pages
 {
+    using Azure.Core;
+    using Azure.Identity;
+    using Azure.Security.KeyVault.Secrets;
+    using Microsoft.Azure.KeyVault;
+    using Microsoft.Extensions.Configuration;
+
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        [BindProperty]
+        public string SecretValue { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
         {
+            SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
+                {
+                    Delay= TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                }
+            };
+            var client = new SecretClient(new Uri(config["KeyVaultUri"]), new DefaultAzureCredential(), options);
+            //client.SetSecret(new KeyVaultSecret("fff", "ddd"));
+            KeyVaultSecret kvSecret = client.GetSecret("TestSecret");
+            SecretValue = kvSecret.Value;
             _logger = logger;
         }
 
